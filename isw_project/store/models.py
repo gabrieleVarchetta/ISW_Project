a83bda1a4c40917879f70069d4b0b9ed01b84ab0
Gab <gabrielevarch@gmail.com>
2023-06-14 23:58:24 +0200

    added models and customer form
    customer, shipping address and residence address

--- isw_project/store/models.py
+++ isw_project/store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_day = models.DateField()
+
+
+class Address(models.Model):
+    country = models.CharField(max_length=256)
+    region = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+    street_address = models.CharField(max_length=256)
+    postal_code = models.CharField(max_length=256)
+    province = models.CharField(max_length=256)
+
+    class Meta:
+        abstract = True
+
+
+class ShippingAddress(Address):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class ResidentialAddress(Address):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
--- isw_project/store/tests.py
+++ isw_project/store/tests.py
@@ -1,3 +1,133 @@
-from django.test import TestCase
-
+import unittest
+from django.test import LiveServerTestCase
+from django.urls import reverse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 # Create your tests here.
+from . import models
+
+
+class TestUseCase006(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_014(self):
+        driver = self.driver
+        driver.get(self.live_server_url+reverse('shopping_cart'))
+        elem = driver.find_element(By.CLASS_NAME, 'da_inserire')
+        elem.send_keys('5')
+        elem.send_keys(Keys.ENTER)
+        self.assertEqual(elem.text, '5')
+        time.sleep(2)
+
+    def test_case_015(self):
+        driver = self.driver
+        driver.get(self.live_server_url+reverse('shopping_cart'))
+        elem = driver.find_element(By.CLASS_NAME, 'da_inserire')
+        elem.send_keys('-1')
+        elem.send_keys(Keys.ENTER)
+        banner = driver.find_element(By.ID, 'da_inserire')
+        self.assertEqual('Invalid quantity.', banner.text)
+        time.sleep(2)
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+class TestUseCase007(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_016(self):
+        driver = self.driver
+        driver.get(self.live_server_url+reverse('shopping_cart'))
+        button = driver.find_element(By.CLASS_NAME, 'da_inserire')
+        products = driver.find_elements(By.CLASS_NAME, 'product')
+        old_len = len(products)
+        button.send_keys(Keys.ENTER)
+        confirm_btn = driver.find_element(By.ID, 'da_inserire')
+        confirm_btn.send_keys(Keys.ENTER)
+        products = driver.find_elements(By.CLASS_NAME, 'product')
+        self.assertEqual(len(products), old_len - 1)
+        time.sleep(2)
+
+    def test_case_017(self):
+        driver = self.driver
+        driver.get(self.live_server_url+reverse('shopping_cart'))
+        button = driver.find_element(By.CLASS_NAME, 'da_inserire')
+        products = driver.find_elements(By.CLASS_NAME, 'product')
+        old_len = len(products)
+        button.send_keys(Keys.ENTER)
+        cancel_btn = driver.find_element(By.ID, 'da_inserire')
+        cancel_btn.send_keys(Keys.ENTER)
+        products = driver.find_elements(By.CLASS_NAME, 'product')
+        self.assertEqual(len(products), old_len)
+        time.sleep(2)
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+class TestUseCase008(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_018(self):
+        pass
+
+    def test_case_019(self):
+        pass
+
+    def test_case_020(self):
+        pass
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+class TestUseCase009(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_021(self):
+        pass
+
+    def test_case_022(self):
+        pass
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+class TestUseCase010(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_023(self):
+        pass
+
+    def test_case_024(self):
+        pass
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+class TestUseCase011(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Safari()
+
+    def test_case_025(self):
+        pass
+
+    def test_case_026(self):
+        pass
+
+    def tearDown(self) -> None:
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()

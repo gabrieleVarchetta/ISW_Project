fe9960735da8dd341d3eb431a3abd22940e8eb32
bugasss <114656578+bugasss@users.noreply.github.com>
2023-06-14 23:56:34 +0200

    First models
    class of Product, Order, ShoppingCart, OrderProduct, CartProduct

--- isw_project/store/models.py
+++ isw_project/store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField()
+    name = models.CharField()
+    decription = models.TextField()
+    category = models.CharField()
+    price = models.FloatField()
+    # Altri campi del prodotto
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    price = models.FloatField()
+    date = models.DateTimeField()
+
+class ShoppingCart(models.Model):
+    products = models.ManyToManyField(Product, through='OrderProduct')
+
+class OrderProduct(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    order = models.ManyToOneRel(Order, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+class CartProduct(models.Model):
+    shopping_cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
--- isw_project/store/tests.py
+++ isw_project/store/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+import unittest
+from django.urls import reverse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+class TestUseCase012(LiveServerTestCase):
+    def setUp(self):
+        self.driver = webdriver.Safari()
+
+    def test_case_027(self):
+        # Accedi come amministratore
+        self.driver.get(self.live_server_url + '/admin/')
+        username_input = self.driver.find_element(By.NAME, 'username')
+        username_input.send_keys('admin')
+        password_input = self.driver.find_element(By.NAME, 'password')
+        password_input.send_keys('password')
+        password_input.send_keys(Keys.ENTER)
+
+        # Vai alla pagina di gestione dei prodotti
+        self.driver.get(self.live_server_url + '/admin/products/')
+        add_product_link = self.driver.find_element(By.LINK_TEXT, 'Aggiungi prodotto')
+        add_product_link.click()
+
+        # Compila il form per aggiungere un nuovo prodotto
+        name_input = self.driver.find_element(By.NAME, 'name')
+        name_input.send_keys('Nuovo prodotto')
+        description_input = self.driver.find_element(By.NAME, 'description')
+        description_input.send_keys('Descrizione del nuovo prodotto')
+        price_input = self.driver.find_element(By.NAME, 'price')
+        price_input.send_keys('10.99')
+
+        # Invia il form
+        save_button = self.driver.find_element(By.NAME, '_save')
+        save_button.click()
+
+        # Verifica che il prodotto sia stato aggiunto correttamente
+        product_link = self.driver.find_element(By.LINK_TEXT, 'Nuovo prodotto')
+        self.assertIsNotNone(product_link)
+    def tearDown(self) -> None:
+        self.driver.close()
+
 
 # Create your tests here.

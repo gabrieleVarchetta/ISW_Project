bbc7bb57f951603e69ee58aa3f4c49665be4741a
bugasss <114656578+bugasss@users.noreply.github.com>
2023-06-21 00:39:49 +0200

    site admin side

--- isw_project/store/admin.py
+++ isw_project/store/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Customer, Product
+from django.db.models.functions import Lower
+
+from .models import Customer, Product, Order
+
+
 # Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(Product)
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ["name", "price", "category"]
+    list_filter = ["category"]
+    search_fields = ["name"]
+
+@admin.register(Order)
+class Order(admin.ModelAdmin):
+    list_display = ["price", "placed_at", "customer"]
+    
+
+
+
--- isw_project/store/models.py
+++ isw_project/store/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.functions import Lower
 
 # Create your models here.
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name='nome prodotto')
     description = models.TextField(max_length=512)
     category = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f'Product: {self.name}, Price: {self.price}'
+        return f'Product: {self.name}'
 
 
 class Customer(models.Model):
